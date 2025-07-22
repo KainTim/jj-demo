7129fd1891d12950d6b79ca7b8c975bd9577b9f5
Tim Kainz <tikaiz@gmx.at>
2025-07-22 23:32:25 +0200

    Updated Tab3 Text

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -7,16 +7,16 @@ const Tab3: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>Tab 4</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
+            <IonTitle size="large">Tab 4</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+        <ExploreContainer name="Tab 4 page" />
       </IonContent>
     </IonPage>
   );
